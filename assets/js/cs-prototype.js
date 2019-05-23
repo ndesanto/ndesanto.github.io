873d9ccff03685f5cf7464f5048d7ce76e429b17
Nick <nick@minutebox.com>
2019-05-23 00:07:04 -0400

    cs) initial cs commit

---
+++ assets/js/cs-prototype.js
@@ -0,0 +1,391 @@
+
+var kitchenList1 = [
+  'Kitchen demolition including cabinets,tile floor,baseboards,backsplash ,countertop,sink,faucet',
+  'FULL GUT OF KITCHEN DRYWALL WALL',
+  'PLASTER FULL GUT WALLS - includes STRAPPING AND INSULATION',
+  'REMOVE BULKHEADS AND INSTALL NEW DRYWALL',
+  'REMOVE DRYWALL OUTSIDE OF KITCHEN DEMO - PER SQ.FT',
+  'REMOVE & DISPOSE OF PLASTER WALLS OUTSIDE OF KITCHEN DEMO - PER SQ.FT',
+  'SCRAPE STUCCO CEILING / PATCH / PRIME - PER SQ.FT'
+]
+
+var kitchenList1Num = [2700, 1000, 2000, 1050, 3, 4, 9]
+var kitchenList1Title = 'Kitchen'
+
+var kitchenList2 = [
+  'REMOVE HARDWOOD / LAMINATE /  PARQUET OUTSIDE OF KITCHEN DEMO - PER SQ.FT',
+  'REMOVE CERAMIC TILE OUTSIDE OF KITCHEN DEMO - PER SQ.FT',
+  'REMOVE & DISPOSE OF CARPET  - PER SQ.FT',
+  'REMOVE & DISPOSE OF VINYL  - PER SQ.FT'
+]
+var kitchenList2Num = [2,4,2,1]
+var kitchenList2Title = ''
+
+var kitchenList3 = [
+  'DISPOSAL FOR BASIC KITCHEN DEMO',
+  'DISPOSAL FOR FULL PLASTER GUT ADDITIONAL TO BASIC DISPOSAL',
+  'DISPOSAL FOR KITCHEN DEMO INCL WALL REMOVAL NON STRUCTURAL',
+  'DISPOSAL FOR KITCHEN DEMO INCL WALL REMOVAL STRUCTURAL',
+  'DISPOSAL CERAMIC TILE OUTSIDE OF KITCHEN DEMO PER SQ.FT',
+  'DISPOSAL OF HARDWOOD / PARQUET OUTSIDE OF KITCHEN DEMO'
+]
+var kitchenList3Num =
+[2,
+4,
+2,
+1,
+2,
+4]
+var kitchenList3Title = 'Disposal'
+
+var kitchenList4 = [
+  'REMOVE VANITY & INSTALL VANITY ',
+  'RE & RE TOILET ( OR INSTALL NEW )',
+  'PATCH  / PREP / PAINT POWDER ROOM',
+  'SUPPLY AND INSTALL MIRROR',
+  'SUPPLY AND INSTALL ACCESSORIES'
+]
+var kitchenList4Num = [
+550,
+250,
+550,
+75,
+75
+]
+var kitchenList4Title = 'Powder Room'
+
+var kitchenList5 = [
+  'BUILD HALF WALL UP TO 8 \' WALL REMOVALS -REMOVAL PRICING TO BE FINALIZED AFTER INSPECTION / ARCT DRAWINGS / PERMITS',
+  'BUILD FULL HEIGHT WALLS DRYWALL COMPLETE  - PER LNFT - MIN 4\'',
+  'BUILD BULKHEAD - PER LNFT - MIN 5 ft',
+  'SUPPLY & INSTALL DOOR ( INCLUDES DOOR UP TO $60.00 -  DOES NOT INCLUDE HARDWARE )',
+  'NON LOAD BEARING WALL REMOVAL UP TO 10\'',
+  'NON LOAD BEARING WALL REMOVAL UP TO 20\'',
+  'LOAD BEARING WALL REMOVAL UP TO 10\'',
+  'LOAD BEARING WALL REMOVAL UP TO 20\' - OVER 20\' NEEDS TO BE QUOTED',
+  'LOAD BEARING WALL REMOVAL UP TO 10\' FLUSH BEAM INSTALLATION',
+  'LOAD BEARING WALL REMOVAL UP TO 20\' FLUSH BEAM INSTALLATION'
+]
+var kitchenList5Num = [1500,
+200,
+100,
+390,
+1800,
+2000,
+6300,
+7800,
+1250,
+1650]
+var kitchenList5Title = ''
+
+var plumbingList = [
+  'MOVE A PLUMBING FIXTURE ( DW,SINK ETC )',
+  'INSTALL ROUGH IN FOR NEW PLUMBING FIXTURE',
+  'SINK AND FAUCET CONNECTION AFTER COUNTERTOP INSTALL',
+  'SET UP & INSTALL APRON SINK',
+  'INSTALL DW AFTER COUNTERTOP INSTALL COMPLETE',
+  'RE & RE GARBURATOR - NO GUARANTEES OF RE-USABILITY - CLIENT TO STORE UNIT',
+  'SUPPLY & INSTALL FRIDGE WATERBOX & LINE',
+  'INSTALL TEMP SINK PRIOR TO COUNTERTOP INSTALL',
+  'TERMINATE PLUMBING FIXTURE AND CAP'
+]
+var plumbingListNum = [520,
+515,
+675,
+275,
+255,
+375,
+380,
+550,
+230]
+var plumbingListTitle = 'Plumbing'
+
+var electricalList = [
+  'ELECTRICAL PERMIT',
+  'SEPARATE PERMIT REQUIRED FOR HEATED FLOOR PANEL CHANGE OR PONY PANEL',
+  'UPGRADE EXISTING ELECTRICAL TO 20 AMP (CURRENT CODE)',
+  'MOVE APPLIANCE LOCATION',
+  'SUPPLY AND INSTALL LED POT LIGHTS',
+  'SUPPLY DEDICATED LINE ( FRIDGE / OTR / WASHING MACHINE / HEATED FLOOR )',
+  'SUPPLY AND INSTALL ARC FAULT LINE',
+  'ADD NEW LINE-WILL BE REQUIRED IF MORE THAN 4 HALOGEN & 10 LED POT LIGHTS',
+  'SUPPLY STOVE LINE / DRYER',
+  'INSTALL ISLAND PENDANT LIGHTS ( INCLUDES ROUGH IN & FIXTURE INSTALLATION )',
+  'SUPPLY & INSTALL UNDER CABINET LIGHTING',
+  'SUPPLY & INSTALL DIMMER SWITCH',
+  'SUPPLY & INSTALL DECORA SWITCH OR RECEPTACLE - REPLACING EXISTING',
+  'SUPPLY & INSTALL NEW 100 AMP PANEL',
+  'SUPPLY & INSTALL NEW 100 AMP PANEL IN CONDO OR APARTMENT',
+  'SUPPLY AND INSTALL PONY PANEL',
+  'SUPPLY AND INSTALL CEILING LIGHT FIXTURE - BASIC LIGHT FIXTURES -NOT CHANDELIERS',
+  'SUPPLY AND INSTALL SMALL CEILING FAN',
+  'SUPPLY AND INSTALL LARGE CEILING FAN- INVESTIGATON REQUIRED TO ASSESS HOW SUPPORT CAN BE INSTALLED',
+  'TERMINATE AN ELECTRICAL FIXTURE OR LINE / MOVE SWITCHES TO OTHER WALL'
+]
+var electricalListNum = 
+[200,
+200,
+1300,
+340,
+205,
+260,
+450,
+260,
+490,
+490,
+1300,
+75,
+50,
+1800,
+2400,
+2050,
+125,
+200,
+780,
+240]
+
+var electricalListTitle = 'Electrical'
+
+var hvacList1 = [
+  'PREP & INSTALL NEW VENTING FOR RANGE HOOD - UNDER 10\'',
+  'PREP & INSTALL NEW VENTING FOR RANGE HOOD - BETWEEN 10-20\'',
+  'INSTALL OTR',
+  'INSTALL DECORATIVE RANGE HOOD',
+  'MOVE / MODIFY HEAT REGISTER TO VENT UNDER TOE KICK ( OPEN FROM BELOW)',
+  'MOVE THERMOSTAT',
+  'SUPPLY AND INSTALL GAS LINE ( OPEN FROM BELOW AND GAS ACCESSIBLE )',
+  'SUPPLY AND INSTALL GAS LINE NOT OPEN FROM BELOW',
+  'TERMINATE A GAS FIXTURE / GAS LINE & CAP',
+  'TERMINATE VENTING AND CAP EXTERIOR'
+]
+var hvacList1Num = [
+750,
+1000,
+430,
+775,
+590,
+430,
+590,
+0,
+515,
+225]
+var hvacList1Title = 'HVAC'
+
+var hvacList2 = [
+  'SUPPLY AND INSTALL NEW DRYWALL  / TAPE  / SAND /PER SQ.FT',
+  'PLASTER - INSTALL NEW FRAMING / DRYWALL  / SAND /PER SQ.FT',
+  'SUPPLY & INSTALL INSULATION & VAPOUR BARRIER',
+  'PATCH / PREP WALLS IF NOT FULL GUT',
+  'PATCH / PREP WALLS IF FULL GUT',
+  'PRIME/PAINT WALLS / CEILING / TRIM UP TO 150 SQ.FT ROOM',
+  'PRIME/PAINT WALLS / CEILING / TRIM OVER 150 SQ.FT - PER  SQ.FT OF ROOM',
+  'WALLPAPER REMOVAL - QUOTE ONLY'
+]
+var hvacList2Num = 
+[9,
+16,
+4,
+600,
+1100,
+900,
+6]
+var hvacList2Title = ''
+
+var flooringList = [
+  'SUPPLY & INSTALL CERAMIC TILE UP TO 12 X 24 TILE ( ALLOWANCE OF $ 3.00 SQ.FT )',
+  '(includes supply & install of 1/4\" or 1/2\" cement board as required - OVER 12 X 24 QUOTE ONLY )',
+  'SUPPLY & INSTALL POLISHED PORELAIN OR STONE UP TO 12 X 24  ( ALLOWANCE UP TO $ 6.00 SQ.FT )',
+  'SUPPLY & INSTALL SOUND PROOF UNDERLAY FOR TILE - PER SQ.FT ( EASY MAT )',
+  'SUPPLY & INSTALL HARDWOOD FLOORING  PER SQ.FT( ALLOWANCE OF $4.00 SQ.FT )',
+  'SAND/STAIN REFINISH HARDWOOD FLOORING  PER SQ.FT -1 COAT STAIN-2 COATS FINISH',
+  'SUPPLY & INSTALL LAMINATE FLOORING  PER SQ.FT( ALLOWANCE OF $2.50 SQ.FT )',
+  'SUPPLY & INSTALL UNDERLAY FOR HARDWOOD OR LAMAINTE - W OR W/O SOUD PROOF PER SQ.FT',
+  'INSTALL VINYL PLANK FLOORING',
+  'SUPPLY & INSTALL PLYWOOD UNDERLAY FOR HARDWOOD INSTALL PER SQ.FT',
+  'SUPPLY & INSTALL 4 1/4\" BASEBOARD  (ALLOWANCE OF $ 2.00 LN.FT )',
+  'SUPPLY & INSTALL 1/4 ROUND  (ALLOWANCE OF $ 1.50 LN.FT )',
+  'SUPPLY & INSTALL 5\" BASEBOARD  (ALLOWANCE OF $ 3.00 LN.FT )',
+  'SUPPLY & INSTALL 7\" BASEBOARD  (ALLOWANCE OF $ 4.00 LN.FT )',
+  'SUPPLY & INSTALL STANDARD CASING  (ALLOWANCE OF $ 2.00 LN.FT )',
+  'SUPPLY & INSTALL WOOD THRESHOLDS - PER LNFT',
+  'RE & RE STANDARD DOOR - $60 ALLOWANCE - DOES NOT INCLUDE HARDWARE',
+  'RE & RE CLOSET DOORS',
+  'SUPPLY & INSTALL POCKET DOOR (INCL STNDRD 6 PANEL DOOR - NOT HRDWRE-PAINTING NOT INCL.)',
+  'SUPPLY & INSTALL CROWN MOULDING (ALLOWANCE OF $ 2.50 LN.FT )'				
+]
+
+var flooringListNum = [
+19,
+25,
+56,
+10,
+4,
+7,
+2,
+3,
+3,
+7,
+6,
+9,
+13,
+7,
+15,
+290,
+220,
+1250,
+21
+]
+var flooringListTitle = 'Flooring'
+
+var basicPremKitchen = ['BASIC KITCHEN INSTALL  - PER LNFT', 'PREMIUM KITCHEN INSTALL  - PER LNFT']
+var basicPremKitchenNum = [95,
+115]
+var basicPremKitchenTitle = ' Basic Or Premium Kitchen Install'
+
+var stairsList = [
+  'SUPPLY & INSTALL OAK TREAD & RISER ONLY ON BOXED STEP',
+  'RE & RE WOOD SPINDLES / SUPPLY & INSTALL & STAIN NEW OAK STEPS & RISERS',
+  'S & I NEW OAK TREAD RISER, WOOD SPINDLES,OAK HANDRAIL,2-6\" POSTS,PAINT SPINDLES /STRINGERS & RISERS',
+  'S & I NEW OAK TREAD RISER, WOOD SPINDLES,OAK HANDRAIL,2-6\" POSTS,STAIN SPINDLES /STRINGERS & RISERS',
+  'S & I NEW OAK TREAD RISER, WROUGHT IRON SPINDLES,OAK HANDRAIL,2-6\" POSTS ,PAINT STRINGERS',
+  'S & I NEW OAK TREAD RISER, WROUGHT IRON SPINDLES,OAK HANDRAIL,2-6\" POSTS ,STAIN STRINGERS',
+  'BANISTERS - S &  I NEW OAK HANDRAIL, NOSING ( STAINED )  WITH WROUGH IRON SPINDLES PER LNFT  - MIN 5 LNFT',
+  'BANISTER - S & I NEW OAK HANDRAIL ,NOSING WITH WOOD SPINDLES  - INCLUDES STAIN HAND RAIL & PAINT SPINDLES - PER LNFT - MIN 5 LNFT'
+]
+var stairsListNum = [
+340,
+450,
+495,
+545,
+620,
+750,
+200,
+140
+]
+var stairsListTitle = 'Stairs'
+
+var backSplashList = [
+  'SUPPLY & INSTALL CERAMIC BACKSPLASH( 35 SQ.FT TILE ALWNCE UP TO $5.00 SQ.FT )',
+  'SUPPLY & INSTALL STONE BACKSPLASH  ( 35 SQ.FT TILE ALWNCE UP TO $10.00 SQ.FT )',
+  'SUPPLY & INSTALL STONE BACKSPLASH  ( 35 SQ.FT TILE ALWNCE UP TO $15.00 SQ.FT )',
+  'ADDITIONAL COST PER SQ.FT FOR CERAMIC TILE BACKSPLASH',
+  'ADDITIONAL COST PER SQ.FT FOR STONE / GLASS BACKSPLASH'
+]
+var backSplashListNum = [
+1400,
+1700,
+2150,
+13,
+25
+]
+var backSplashListTitle = 'Backsplash'
+
+var applianceList = [
+  'INSTALL BASIC APPLIANCES  - PLUG IN ONLY - PER APPLIANCE',
+  'INSTALL BASIC RANGE HOOD',
+  'INSTALL WALL OVEN',
+  'INSTALL COOK TOP',
+  'INSTALL GAS STOVE',
+  'INSTALL BUILT IN MICROWAVE',
+  'INSTALL WASHER OR DRYER  - PER APPLIANCE',
+  'INSTALL STACKABLE WASHER & DRYER  -new install - customer to purchase bracket if required',
+  'MOVE FURNITURE / BOXES PER ROOM'
+]
+var applianceListNum = [
+215,
+250,
+425,
+425,
+385,
+425,
+215,
+550,
+200
+]
+var applianceListTitle = 'Appliances'
+
+var condoFees = ['10% OF BASE LABOUR AND MATERIALS WILL BE APPLIED (EXCEPT ELECTRICAL)']
+var condoFeesNum = [7010]
+var condoFeesTitle = 'Condo Fees'
+
+var listOfLists = [kitchenList1, kitchenList2, kitchenList3, kitchenList4, kitchenList5, plumbingList, electricalList, hvacList1, hvacList2, flooringList, basicPremKitchen, stairsList, backSplashList, applianceList, condoFees]
+var listOfListsNum = [kitchenList1Num, kitchenList2Num, kitchenList3Num, kitchenList4Num, kitchenList5Num, plumbingListNum, electricalListNum, hvacList1Num, hvacList2Num, flooringListNum, basicPremKitchenNum, stairsListNum, backSplashListNum, applianceListNum, condoFeesNum]
+var listOfListsTitle = [kitchenList1Title, kitchenList2Title, kitchenList3Title, kitchenList4Title, kitchenList5Title, plumbingListTitle, electricalListTitle, hvacList1Title, hvacList2Title, flooringListTitle, basicPremKitchenTitle, stairsListTitle, backSplashListTitle, applianceListTitle, condoFeesTitle]
+
+var innerPage = document.getElementById("cs-inner-page")
+
+function kitchenSheetPage () {
+  for (let i = 0; i < listOfLists.length; i++) {
+    var newTitle = document.createElement('h2')
+    newTitle.innerHTML = listOfListsTitle[i]
+    var newDiv = document.createElement('div')
+    $(newDiv).addClass('content-wrap')
+
+    for (let j = 0; j < listOfLists[i].length; j++) {
+      var newDivContainer = document.createElement('div')
+      $(newDivContainer).addClass('cs-sec-2-form')
+
+      var newDiv2 = document.createElement('div')
+      newDiv2.innerHTML = listOfLists[i][j].toLowerCase()
+      $(newDiv2).addClass('capitalize')
+      $(newDiv2).addClass('grid-col')
+      var newInput = document.createElement('input')
+      $(newInput).addClass('grid-col')
+      var hRule = document.createElement('hr')
+
+      newDivContainer.appendChild(newDiv2)
+      newDivContainer.appendChild(newInput)
+      newDiv.appendChild(newDivContainer)
+      newDiv.appendChild(hRule)
+
+    }
+    innerPage.appendChild(newTitle)
+    innerPage.appendChild(newDiv)
+  }
+}
+kitchenSheetPage()
+// var elementCount = 0;
+
+
+// function sec4ReplaceList (arr) {
+//   var sec4RemoveList = []
+
+//   for (let i = 0; i < sec4UnorderedList.children.length; i++) {
+//     if (sec4UnorderedList.children[i].tagName == 'LI') { 
+//       sec4RemoveList.push(i)
+//     }
+//   }
+//   for (let i = 0; i < sec4RemoveList.length; i++) {
+//     sec4UnorderedList.children[sec4RemoveList[sec4RemoveList.length - i - 1]].remove()
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     sec4UnorderedList.children[0].after(arr[i][0][3])
+//   }
+// }
+// $(sec3edit).bind('click', function (e) { 
+//   if (sec3edit.innerHTML === 'EDIT' && elementCount < 4) {
+//       elementCount++
+//       startEditEis(sec3edit)
+//       var newP = document.createElement("p"); 
+//       var newContent = document.createTextNode(""); 
+//       newP.appendChild(newContent);
+//       $(newP).addClass('product-sec-3-text-special-subname')
+//       $(newP).addClass('product-sec-3-space')
+//       var currentP = document.getElementById("product-sec-3-text-addition"); 
+//       var parentNode = document.getElementById("product-sec-3-parent");
+//       parentNode.insertBefore(newP, currentP); 
+//       newP.contentEditable = 'true'
+//       newP.setAttribute('id','product-sec-3-p' + elementCount)
+//       newP.focus()
+//       $(newP).keydown(function(e){ 
+//         if (e.keyCode == 13){ 
+//           document.getElementById("product-sec-3-p" + elementCount).contentEditable = 'false'
+//           doneEditEis(sec3edit)
+//           e.preventDefault(); 
+//       } 
+//     })
+//   } else if (elementCount < 5){
+//       document.getElementById("product-sec-3-p" + elementCount).contentEditable = 'false'
+//       doneEditEis()
+//   }
+// })

13903efebce01ac83850770aa88bcaee8232e26e
Nick <nick@minutebox.com>
2019-06-07 14:27:39 -0400

    small fixes

--- assets/js/Avenue Prototype.js
+++ assets/js/Avenue Prototype.js
@@ -16,6 +16,7 @@ var demolition = [
 ]
 
 var livingRoom = [
+    'Living Room',
     ['2.5 Inch Cordless Blinds', qty,105,['72x48"','18x72"','24x72"','30x72"','36x72"','42x72"','48x72"','60x48"','60x72"','72x72"','36x48"'],['Esspresso','White'],false],
     ['Vertical Blinds', qty,105,['78x84"','66x84"'],['White'],false],
     ['Balcony Door', qty,[200,115],null,null,vrb],
@@ -36,12 +37,13 @@ var livingRoom = [
 ]
 
 var kitchen = [
+    'Kitchen',
     ['Angle Stop', qty,75,null,null,vrb],
     ['Boiler Pan', qty,'custom',null,null,vrb],
     ['Cabinets and Drawer Pulls', qty,[20,'custom'],null,null,vrb],
     ['Replace Cabinet Door Hinges', qty,350,null,null,false],
     ['Replace Cabinets and Drawers', qty,[1400,1800,2100],['Small','Medium','Large'],null,false],
-    ['Install Countertop', qty,420,['10\'','8\'','6\','4\''],null,false],
+    ['Install Countertop', qty,420,['10\'','8\'','6\'','4\''],null,false],
     ['Miter Kitchen Countertop', qty,220,null,null,false],
     ['Replace Faucet', qty,70,null,null,false],
     ['Flooring', qty,[2.1,'custom'],null,null,vrb],
@@ -54,7 +56,7 @@ var kitchen = [
     ['Replace Sink and Faucet', qty,420,['Glacier Bay','Moen'],null,false],
     ['Replace Baseboard', qty,4.2,null,null,false],
     ['Replace Door Trim', qty,4.2,null,null,false],
-    ['Walls', qty,[35,8.4],['Repair','Replace',null,vrb],
+    ['Walls', qty,[35,8.4],['Repair','Replace'],null,vrb],
     ['Replace Ceiling', qty,5,null,null,false],
     ['Kitchen Backsplash Install', qty,22,null,null,false],
     ['Repair Dishwasher', qty,75,null,null,false],
@@ -62,10 +64,6 @@ var kitchen = [
     ['Stove', qty,105,['White 24W x 42 1/8H x 26 5/8D (in.)','White 30W x 46 1/2H x 29 7/8D (in.)','Stainless Steel 30W x 46 7/8H x 26 1/4D (in.)'],null,false],
     ['Dishwasher', qty,640,['White 32 1/2H x 17 19/32W x 22 13/32D (in.)','White 32 1/2H x 23 1/2W x 22 13/32D (in.)','Stainless Steel 32 1/2H x 23 1/2W x 22 13/32D (in.)'],null,false]
 ]
-
-//var testObject = ['Words', Quantity: user-input, Price: Int, Size [], Variant [], Variable ]
-//repair/replace
-
 
 var bedrooms = [
     "Bedroom(s)",

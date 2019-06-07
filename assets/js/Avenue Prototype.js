827977c16a21b848ebc338ffc6544c9310e9a817
Patterston <pnavaratnam@cerastoneltd.com>
2019-06-07 14:20:26 -0400

    message

---
+++ assets/js/Avenue Prototype.js
@@ -0,0 +1,64 @@
+var rowObject = [ 'Words', Quantity, Price, Size, Variant, variable]
+
+var preConstruction = [
+    'Pre Construction Prep',
+    ['Protect Flooring With RAM board', qty,600,null,null,false],
+    ['Clean Suite - Move in ready condition', qty,550,null,null,false],
+    ['Cover Stairs - with RAM board', qty,100,null,null,vrb],
+    ['Cover Hallways/Carpet Stairs - Roberts Carpet Protection Film', qty,190,null,null,vrb]
+]
+
+var demolition = [
+    'Demolition',
+    ['Classic Suite Demolition', qty,1200,null,null,false],
+    ['Semi-Premium Demolition', qty,1800,null,null,false],
+    ['Premium Suite Demolition', qty,2300,null,null,false],
+]
+
+var livingRoom = [
+    ['2.5 Inch Cordless Blinds', qty,105,['72x48"','18x72"','24x72"','30x72"','36x72"','42x72"','48x72"','60x48"','60x72"','72x72"','36x48"'],['Esspresso','White'],false],
+    ['Vertical Blinds', qty,105,['78x84"','66x84"'],['White'],false],
+    ['Balcony Door', qty,[200,115],null,null,vrb],
+    ['Replace Ceiling Fan', qty,140,null,null,false],
+    ['Replace Crown Molding', qty,14,null,null,false],
+    ['Flooring', qty,[2.1,"custom"],null,null,vrb],
+    ['Replace Light Fixture', qty,42,null,null,false],
+    ['Replace Stairs', qty,140, null,null, false],
+    ['Replace Baseboard', qty,4.2,null,null,false],
+    ['Replace Door Trim', qty,4.2,null,null,false],
+    ['Replace Receptacles', qty,35,null,null,false],
+    ['Replace Switches', qty,35,null,null,false],
+    ['Walls', qty,[8.4,35],null,null,vrb],
+    ['Replace Window Screens', qty,28,null,null,false],
+    ['Screw Down Flooring', qty,0.8,null,null,false],
+    ['Patch and Replace Stucco Ceiling', qty,[1000,400],null,null,vrb],
+    ['Replace Door', qty,200,['16"','32"','30"','28"','24"'],['Left Handed','Right Handed'],false],
+]
+
+var kitchen = [
+    ['Angle Stop', qty,75,null,null,vrb],
+    ['Boiler Pan', qty,'custom',null,null,vrb],
+    ['Cabinets and Drawer Pulls', qty,[20,'custom'],null,null,vrb],
+    ['Replace Cabinet Door Hinges', qty,350,null,null,false],
+    ['Replace Cabinets and Drawers', qty,[1400,1800,2100],['Small','Medium','Large'],null,false],
+    ['Install Countertop', qty,420,['10\'','8\'','6\','4\''],null,false],
+    ['Miter Kitchen Countertop', qty,220,null,null,false],
+    ['Replace Faucet', qty,70,null,null,false],
+    ['Flooring', qty,[2.1,'custom'],null,null,vrb],
+    ['Replace Light Fixture', qty,42,null,null,false],
+    ['Replace GFCI', qty,105,null,null,false],
+    ['Replace Microwave', qty,70,null,null,false],
+    ['Replace Range Hood', qty,210,null,null,false],
+    ['Install Charcoal Filtered Range Hood', qty,450,null,null,false]
+    ['Replace Sink', qty,420,null,null,false],
+    ['Replace Sink and Faucet', qty,420,['Glacier Bay','Moen'],null,false],
+    ['Replace Baseboard', qty,4.2,null,null,false],
+    ['Replace Door Trim', qty,4.2,null,null,false],
+    ['Walls', qty,[35,8.4],['Repair','Replace',null,vrb],
+    ['Replace Ceiling', qty,5,null,null,false],
+    ['Kitchen Backsplash Install', qty,22,null,null,false],
+    ['Repair Dishwasher', qty,75,null,null,false],
+    ['Refridgerator', qty,105,['White 59 7/8H x 24W x 28 5/8D (in.)','White 66 5/8H x 29 1/2W x 30 15/16D (in.)','White 61 3/4H x 28W x 31 5/8D (in.)','Stainless Steel 66 5/8H x 29 1/2W x 30 15/16D (in.)'],null,false],
+    ['Stove', qty,105,['White 24W x 42 1/8H x 26 5/8D (in.)','White 30W x 46 1/2H x 29 7/8D (in.)','Stainless Steel 30W x 46 7/8H x 26 1/4D (in.)'],null,false],
+    ['Dishwasher', qty,640,['White 32 1/2H x 17 19/32W x 22 13/32D (in.)','White 32 1/2H x 23 1/2W x 22 13/32D (in.)','Stainless Steel 32 1/2H x 23 1/2W x 22 13/32D (in.)'],null,false]
+]
